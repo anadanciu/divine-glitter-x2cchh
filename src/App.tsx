7429c08b99b6639fbfd9e2fc954466fe31097bc2
Anamaria Nanes <anamaria.danciu.ad@gmail.com>
2025-10-24 13:17:51 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import "./styles.css";
+import reactImg from "./assets/react.svg";
+
+const reactDescriptions: string[] = ["Fundamental", "Core", "Crucial"];
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header className="App">
+      <img src={reactImg} alt="react Logo" />
+      <h1>Hello CodeSandbox</h1>
+      <h2> {description} React concepts you will need to know</h2>
+    </header>
+  );
+}
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
